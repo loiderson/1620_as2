6c9db0d1b403835469d17cf51a7d45c224c6e449
Jacob Lloyd <jlloyd47@my.bcit.ca>
2022-04-11 16:08:22 -0700

    Inside the src/app.js file, I created two functions. One function creates a new <textarea> when the plus icon is clicked on. When clicked on, using insertAdjacentHTML, a save and cancel button are also created.
    The Second function allows the user to press the cancel button, and the
    <textarea> closes. So does the save and cancel buttons, to prevent
    clutter. The main method used to remove them when clicked, is:
    removeChild().

--- src/app.js
+++ src/app.js
@@ -5,3 +5,57 @@ const notes = [
     id: 1 
   }
 ]
+
+const TextArea = `
+<textarea class="NoteTaker" rows="35" cols="50"></textarea>
+`
+
+const saveBtn = `
+<button class="SaveButton" type="button">
+  Save
+</button>
+`
+
+const cancelBtn = `
+<button class="CancelButton" onclick="CancelNote()">Cancel</button>
+`
+
+const closeBtn = `
+<button class="CloseButton" type="button">
+  Close
+</button>
+`
+
+const iconCreate = document.querySelector('.fa-solid.fa-circle-plus')
+const iconsClass = document.querySelector('.icons')
+const writeNoteArea = document.querySelector('.write-note-area')
+var cancelBtnVari = document.querySelector('.CancelButton')
+const textAreaVar = document.querySelector('.NoteTaker')
+
+function NoteTakeCreate(evt) {
+  if (evt.target.localName === "i") {
+    writeNoteArea.insertAdjacentHTML('afterbegin', TextArea)
+    writeNoteArea.insertAdjacentHTML('afterbegin', cancelBtn)
+    writeNoteArea.insertAdjacentHTML('afterbegin', saveBtn)
+    cancelBtnVari = document.querySelector('.CancelButton')
+  }
+}
+iconsClass.addEventListener('click', NoteTakeCreate)
+
+
+
+//function CancelNote() {
+  //text = document.getElementsByClassName("NoteTaker")
+  //text.remove()
+  //(writeNoteArea.firstChild.cancelBtn('click'))
+//}
+//cancelBtnVari.addEventListener('click', CancelNote)
+
+function CancelNote() {
+  var text = document.querySelector('.NoteTaker')
+  text.parentElement.removeChild(text)
+  var saveBtnVari = document.querySelector('.SaveButton')
+  saveBtnVari.parentElement.removeChild(saveBtnVari)
+  cancelBtnVari.parentElement.removeChild(cancelBtnVari)
+}
+
